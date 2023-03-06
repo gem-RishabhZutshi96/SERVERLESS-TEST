cb5bc7c51f03506750fad6877ff5fe8c33d570f7
Rishabh Zutshi <Rishabh.Zutshi@Geminisolutions.com>
2023-03-07 00:06:05 +0530

    AUTH

---
+++ services/auth/apis/verifyLogin.js
@@ -0,0 +1,81 @@
+import { internalServer, badRequest } from "../../utilities/response/index";
+import axios from 'axios';
+import { getUserRole } from "../../utilities/misc/getRole";
+import * as jwt from 'jsonwebtoken';
+import { parameterStore } from '../../utilities/config/commonData';
+import { devLogger, errorLogger } from "../utils/log-helper";
+export const verifyLogin = async(event) => {
+    try {
+      devLogger("verifyLogin", event, "event");
+      if(!(event.body.accessToken || event.body.email)){
+        return badRequest("🤔🤔 Missing body parameters");
+      } else {
+        const { accessToken, email } = event.body;
+        const response = await verifyUser(accessToken, email);
+        return response;
+      }
+    } catch(err) {
+      errorLogger("verifyLogin", err, "Error db call");
+      return internalServer(`Error in DB `);
+    }
+};
+
+/**
+ * Verify MSAl access token through function call and compare email of logged in user and user
+ * email from graph API
+ * @param  token     MSAL access token
+ * @param  msalMail  User email recieved from SSO object
+ */
+async function verifyUser(token, msalMail) {
+    const { email, success } = await sendMicrosoftAuthenticationRequest(token);
+    if (success && (email === msalMail)) {
+      const token = await generateToken(email);
+      const role = await getUserRole(email);
+      return {
+        statusCode:200,
+        data: { token, role },
+        success: true,
+        message: '😎😎 Verification successful',
+      };
+    } else {
+      throw new Error('Invalid user');
+    }
+};
+
+  /**
+ * Verify MSAl access token through graph API
+ * @param  token MSAL access token
+ */
+async function sendMicrosoftAuthenticationRequest(token) {
+    const msalReqObj = {
+      method: 'get',
+      url: parameterStore[process.env.stage].msalServiceUrl,
+      headers: { 'Authorization': token }
+    };
+    let res = await axios(msalReqObj);
+    if (res.data.error)
+      return {
+        success: false,
+        email: null,
+        error: res.data.error,
+      };
+    else
+      return {
+        success: true,
+        email: res.data.mail,
+        error: null
+      };
+};
+
+async function generateToken(email) {
+    const today = new Date();
+    const exp = new Date(today);
+    exp.setDate(today.getDate() + 60);
+    return jwt.sign(
+      {
+        email: email,
+        exp: exp.getTime() / 1000,
+      },
+      parameterStore[process.env.stage].JWT_SECRET
+    );
+  }
